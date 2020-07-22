e5f298853c23f9ffc046400f1ae9522c375f1f8e
Ruan Badenhorst <36207505+nauticafs31@users.noreply.github.com>
2020-07-22 16:31:40 -0400

    Add files via upload

---
+++ assets/theme.js
@@ -0,0 +1,81 @@
+// Place in your app's '/assets' directory 
+window.theme = {
+    accent:"#FF8827",
+    accent_negative:"#ff2c6d",
+    accent_positive:"#33ffe6",
+    background_content:"#F7F9F9",
+    background_page:"#eaeaea",
+    border:"#D1D1D1",
+    breakpoint_font:"1200px",
+    breakpoint_stack_blocks:"700px",
+    card_border:{
+         width:"0px",
+         style:"solid",
+         radius:"0px",
+         color:"#D1D1D1"
+    },
+    card_background_color:"#F7F9F9",
+    card_box_shadow:"0px 1px 3px rgba(0,0,0,0.12), 0px 1px 2px rgba(0,0,0,0.24)",
+    card_margin:"5px",
+    card_padding:"3px",
+    card_outline:{
+         width:"0px",
+         style:"solid",
+         color:"#D1D1D1"
+    },
+    card_header_border:{
+         width:"0px",
+         style:"solid",
+         radius:"0px",
+         color:"#D1D1D1"
+    },
+    card_header_background_color:"#F7F9F9",
+    card_header_box_shadow:"0px 0px 0px rgba(0,0,0,0)",
+    card_header_margin:"0px",
+    card_header_padding:"7px",
+    colorway:[
+         "#ff8827",
+         "#66c2a5",
+         "#8da0cb",
+         "#e78ac3",
+         "#a6d854",
+         "#ffd92f",
+         "#e5c494",
+         "#b3b3b3"
+    ],
+    colorscale:[
+         "#ff8827",
+         "#ff9542",
+         "#ffa35b",
+         "#ffb072",
+         "#ffbd89",
+         "#ffcaa0",
+         "#ffd7b8",
+         "#ffe4cf",
+         "#fff2e7",
+         "#ffffff"
+    ],
+    font_family:"Poppins",
+    font_size:"17px",
+    font_size_smaller_screen:"15px",
+    font_family_header:"Dosis",
+    font_size_header:"24px",
+    font_family_headings:"Lato",
+    font_headings_size:null,
+    header_border:{
+         width:"0px",
+         style:"solid",
+         radius:"0px",
+         color:"#D1D1D1"
+    },
+    header_background_color:"#F7F9F9",
+    header_box_shadow:"0px 1px 3px rgba(0,0,0,0.12), 0px 1px 2px rgba(0,0,0,0.24)",
+    header_margin:"0px 0px 15px 0px",
+    header_padding:"0px",
+    text:"#6D6D6D",
+    report_font_family:"Computer Modern",
+    report_font_size:"12px",
+    report_background_page:"white",
+    report_background_content:"#FAFBFC",
+    report_text:"black"
+}
